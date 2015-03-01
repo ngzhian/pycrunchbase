a464ebce5fa72ec876032ad683b2d8115a3b00fb
Ng Zhi An <ngzhian@gmail.com>
2015-03-01 20:50:02 +0000

    Replace httpretty with mocking requests.get

--- tests/test_pycrunchbase.py
+++ tests/test_pycrunchbase.py
@@ -9,8 +9,6 @@ import json
 
 from requests.exceptions import HTTPError
 
-import httpretty
-
 from pycrunchbase import CrunchBase, Relationship, Page
 
 MOCK_RELATIONSHIP_PAGE = '''{
@@ -88,25 +86,21 @@ class CrunchBaseTestCase(TestCase):
         with self.assertRaises(ValueError):
             CrunchBase(None)
 
-    @httpretty.activate
-    def test_search_for_organization_fails(self):
-        httpretty.register_uri(
-            httpretty.GET,
-            'https://api.crunchbase.com/v/2/organizations?'
-            'user_key=123&name=organization',
-            body='''{
-            }''')
+    @patch('pycrunchbase.pycrunchbase.requests.get')
+    def test_search_for_organization_fails(self, mock_get):
+        mock_get.return_value = Mock(json=Mock(return_value={}))
+
         cb = CrunchBase('123')
         data = cb.organizations('organization')
+
+        mock_get.assert_called_with(
+            'https://api.crunchbase.com/v/2/organizations?'
+            'name=organization&user_key=123')
         self.assertIsNone(data)
 
-    @httpretty.activate
-    def test_get_crunchbase_organization_returns_organization(self):
-        httpretty.register_uri(
-            httpretty.GET,
-            'https://api.crunchbase.com/v/2/organization/organization?'
-            'user_key=123',
-            body='''{
+    @patch('pycrunchbase.pycrunchbase.requests.get')
+    def test_get_crunchbase_organization_returns_organization(self, mock_get):
+        mock_json = make_mock_response_json({
                 "data": {
                     "uuid": "df6628127f970b439d3e12f64f504fbb",
                     "type": "Organization",
@@ -115,226 +109,202 @@ class CrunchBaseTestCase(TestCase):
                         "short_description": "short description"
                     }
                 }
-            }''')
+            })
+        mock_get.return_value = mock_json
+
         cb = CrunchBase('123')
         organization = cb.organization('organization')
+
+        mock_get.assert_called_with(
+            'https://api.crunchbase.com/v/2/organization/organization?'
+            'user_key=123')
         self.assertEqual(organization.description, "Description")
 
-    @httpretty.activate
-    def test_get_crunchbase_organization_fails(self):
-        httpretty.register_uri(
-            httpretty.GET,
-            'https://api.crunchbase.com/v/2/organization/organization?'
-            'user_key=123',
-            body='''{
-            }''')
+    @patch('pycrunchbase.pycrunchbase.requests.get')
+    def test_get_crunchbase_organization_fails(self, mock_get):
+        mock_json = make_mock_response_json({})
+        mock_get.return_value = mock_json
+
         cb = CrunchBase('123')
         data = cb.organization('organization')
+
+        mock_get.assert_called_with(
+            'https://api.crunchbase.com/v/2/organization/organization?'
+            'user_key=123')
         self.assertIsNone(data)
 
-    @httpretty.activate
-    def test_search_for_organization_returns_a_list_of_matches(self):
-        httpretty.register_uri(
-            httpretty.GET,
-            'https://api.crunchbase.com/v/2/organizations?'
-            'user_key=123&name=organization',
-            body='''{
-             "metadata": {
-              "image_path_prefix": "https://example.com/",
-              "www_path_prefix": "https://www.crunchbase.com/",
-              "api_path_prefix": "https://api.crunchbase.com/v/2/",
-              "version": 2
-             },
-             "data": {
-              "paging": {
-               "items_per_page": 1000,
-               "current_page": 1,
-               "number_of_pages": 1,
-               "next_page_url": null,
-               "prev_page_url": null,
-               "total_items": 6,
-               "sort_order": "custom"
-              },
-              "items": [
-               {
-                "updated_at": 1415895087,
-                "created_at": 1371717055,
-                "path": "organization/organization",
-                "name": "organization",
-                "type": "Organization"
-               },
-               {
-                "updated_at": 1415768560,
-                "created_at": 1310530681,
-                "path": "organization/organization2",
-                "name": "organization2",
-                "type": "Organization"
-               }
-              ]
-             }
-            }''')
-        httpretty.register_uri(
-            httpretty.GET,
-            'https://api.crunchbase.com/v/2/organization/organization?'
-            'user_key=123',
-            body='''{
-             "data": {
-              "properties": {
-                  "description": "Description",
-                  "short_description": "short description"
-              },
-              "relationships": {
-               "news": {
-                "paging": {
-                 "total_items": 1,
-                 "first_page_url": "https://api.crunchbase.com/v/2/\
-                                    organization/organization/news",
-                 "sort_order": "created_at DESC"
+    @patch('pycrunchbase.pycrunchbase.requests.get')
+    def test_search_for_organization_returns_a_list_of_matches(self, mock_get):
+        mock_json = make_mock_response_json({
+            "metadata": {
+                "image_path_prefix": "https://example.com/",
+                "www_path_prefix": "https://www.crunchbase.com/",
+                "api_path_prefix": "https://api.crunchbase.com/v/2/",
+                "version": 2
                 },
+            "data": {
+                "paging": {
+                    "items_per_page": 1000,
+                    "current_page": 1,
+                    "number_of_pages": 1,
+                    "next_page_url": None,
+                    "prev_page_url": None,
+                    "total_items": 6,
+                    "sort_order": "custom"
+                    },
                 "items": [
-                 {
-                  "url": "https://example.com",
-                  "author": null,
-                  "posted_on": null,
-                  "type": "PressReference",
-                  "title": "Article Title",
-                  "created_at": 1393522999,
-                  "updated_at": 2014
-                 }
-                ]
-               }
-              }
-              }}''')
+                    {
+                        "updated_at": 1415895087,
+                        "created_at": 1371717055,
+                        "path": "organization/organization",
+                        "name": "organization",
+                        "type": "Organization"
+                        },
+                    {
+                        "updated_at": 1415768560,
+                        "created_at": 1310530681,
+                        "path": "organization/organization2",
+                        "name": "organization2",
+                        "type": "Organization"
+                        }
+                    ]
+                }
+            })
+        mock_get.return_value = mock_json
 
         cb = CrunchBase('123')
         orgs = cb.organizations('organization')
+        mock_get.assert_called_with(
+            'https://api.crunchbase.com/v/2/organizations?'
+            'name=organization&user_key=123')
+
         self.assertIsInstance(orgs, Page)
 
     def test_exception_raised_when_making_calls(self):
         with patch('pycrunchbase.pycrunchbase.requests') as mock_request:
             mock_request.get = Mock(side_effect=HTTPError())
-            httpretty.register_uri(
-                httpretty.GET,
-                'https://api.crunchbase.com/v/2/organizations?'
-                'user_key=123&name=organization',
-                body='''{
-                }''')
             cb = CrunchBase('123')
             with self.assertRaises(HTTPError):
                 cb.organizations('organization')
 
-    @httpretty.activate
-    def test_get_funding_round_data(self):
-        httpretty.register_uri(
-            httpretty.GET,
-            'https://api.crunchbase.com/v/2/funding-round/uuid1'
-            '?user_key=123',
-            body='''{
-                "data": {
-                    "uuid": "uuid1",
-                    "type": "FundingRound",
-                    "properties": {
-                        "funding_type": "private_equity",
-                        "money_raised": 1000000
-                    }
+    @patch('pycrunchbase.pycrunchbase.requests.get')
+    def test_get_funding_round_data(self, mock_get):
+        mock_json = make_mock_response_json({
+            "data": {
+                "uuid": "uuid1",
+                "type": "FundingRound",
+                "properties": {
+                    "funding_type": "private_equity",
+                    "money_raised": 1000000
                 }
-            }''')
+            }
+        })
+        mock_get.return_value = mock_json
+
         cb = CrunchBase('123')
         organization = cb.funding_round('uuid1')
+
+        mock_get.assert_called_with(
+            'https://api.crunchbase.com/v/2/funding-round/uuid1'
+            '?user_key=123')
         self.assertEqual(organization.funding_type, "private_equity")
         self.assertEqual(organization.money_raised, 1000000)
 
-    @httpretty.activate
-    def test_get_person_data(self):
-        httpretty.register_uri(
-            httpretty.GET,
-            'https://api.crunchbase.com/v/2/person/name'
-            '?user_key=123',
-            body='''{
-                "data": {
-                    "uuid": "uuid1",
-                    "type": "Person",
-                    "properties": {
-                        "last_name": "Last",
-                        "first_name": "First"
-                    }
+    @patch('pycrunchbase.pycrunchbase.requests.get')
+    def test_get_person_data(self, mock_get):
+        mock_json = make_mock_response_json({
+            "data": {
+                "uuid": "uuid1",
+                "type": "Person",
+                "properties": {
+                    "last_name": "Last",
+                    "first_name": "First"
                 }
-            }''')
+            }
+        })
+        mock_get.return_value = mock_json
+
         cb = CrunchBase('123')
         person = cb.person('name')
+
         self.assertEqual(person.last_name, "Last")
         self.assertEqual(person.first_name, "First")
+        mock_get.assert_called_with(
+            'https://api.crunchbase.com/v/2/person/name'
+            '?user_key=123')
 
-    @httpretty.activate
-    def test_get_product_data(self):
-        httpretty.register_uri(
-            httpretty.GET,
-            'https://api.crunchbase.com/v/2/product/permalink1'
-            '?user_key=123',
-            body='''{
-                "data": {
-                    "uuid": "uuid1",
-                    "type": "Person",
-                    "properties": {
-                        "lifecycle_stage": "Stage",
-                        "permalink": "permalink1"
-                    }
+    @patch('pycrunchbase.pycrunchbase.requests.get')
+    def test_get_product_data(self, mock_get):
+        mock_json = make_mock_response_json({
+            "data": {
+                "uuid": "uuid1",
+                "type": "Person",
+                "properties": {
+                    "lifecycle_stage": "Stage",
+                    "permalink": "permalink1"
                 }
-            }''')
+            }
+        })
+        mock_get.return_value = mock_json
+
         cb = CrunchBase('123')
         product = cb.product('permalink1')
+
         self.assertEqual(product.lifecycle_stage, "Stage")
         self.assertEqual(product.permalink, "permalink1")
+        mock_get.assert_called_with(
+            'https://api.crunchbase.com/v/2/product/permalink1'
+            '?user_key=123')
 
-    @httpretty.activate
-    def test_get_acquisition_data(self):
-        httpretty.register_uri(
-            httpretty.GET,
-            'https://api.crunchbase.com/v/2/acquisition/uuid1'
-            '?user_key=123',
-            body='''{
-                "data": {
-                    "uuid": "uuid1",
-                    "type": "Person",
-                    "properties": {
-                        "disposition_of_acquired": "Combined",
-                        "acquisition_type": "Acqui-hire"
-                    }
+    @patch('pycrunchbase.pycrunchbase.requests.get')
+    def test_get_acquisition_data(self, mock_get):
+        mock_json = make_mock_response_json({
+            "data": {
+                "uuid": "uuid1",
+                "type": "Person",
+                "properties": {
+                    "disposition_of_acquired": "Combined",
+                    "acquisition_type": "Acqui-hire"
                 }
-            }''')
-        cb = CrunchBase('uuid1')
+            }
+        })
+        mock_get.return_value = mock_json
+
+        cb = CrunchBase('123')
         acquisition = cb.acquisition('uuid1')
+
         self.assertEqual(acquisition.disposition_of_acquired, "Combined")
         self.assertEqual(acquisition.acquisition_type, "Acqui-hire")
+        mock_get.assert_called_with(
+            'https://api.crunchbase.com/v/2/acquisition/uuid1'
+            '?user_key=123')
 
 
 class LoadMoreTestCase(TestCase):
     def setUp(self):
         self.cb = CrunchBase('123')
 
-    @httpretty.activate
-    def test_more_from_relationship_summary_returns_error(self):
+    @patch('pycrunchbase.pycrunchbase.requests.get')
+    def test_more_from_relationship_summary_returns_error(self, mock_get):
         """Load more relationship data from summary"""
-        httpretty.register_uri(
-            httpretty.GET,
-            'https://api.crunchbase.com/v/2/organization/example/past_team'
-            '?user_key=123',
-            body=json.dumps({'data': {'error': 'error'}}))
+        mock_json = make_mock_response_json(
+                {'data': {'error': 'error'}})
+        mock_get.return_value = mock_json
 
         rs = Relationship('past_team', json.loads(PAST_TEAM_RELATIONSHIP))
 
         rs = self.cb.more(rs)
         self.assertIsNone(rs)
-
-    @httpretty.activate
-    def test_more_relationship_for_relationship_summary(self):
-        """Load more relationship data from summary"""
-        data = {'data': json.loads(MOCK_RELATIONSHIP_PAGE)}
-        httpretty.register_uri(
-            httpretty.GET,
+        mock_get.assert_called_with(
             'https://api.crunchbase.com/v/2/organization/example/past_team'
-            '?user_key=123',
-            body=json.dumps(data))
+            '?user_key=123')
+
+    @patch('pycrunchbase.pycrunchbase.requests.get')
+    def test_more_relationship_for_relationship_summary(self, mock_get):
+        """Load more relationship data from summary"""
+        mock_json = make_mock_response_json(
+            {'data': json.loads(MOCK_RELATIONSHIP_PAGE)})
+        mock_get.return_value = mock_json
 
         rs = Relationship('past_team', json.loads(PAST_TEAM_RELATIONSHIP))
 
@@ -342,16 +312,36 @@ class LoadMoreTestCase(TestCase):
         self.assertIsNotNone(rs)
         self.assertEquals(3, len(rs))
 
-    @httpretty.activate
-    def test_more_relationship_for_relationship_page2(self):
+        mock_get.assert_called_with(
+            'https://api.crunchbase.com/v/2/organization/example/past_team'
+            '?user_key=123')
+
+    @patch('pycrunchbase.pycrunchbase.requests.get')
+    def test_more_relationship_for_relationship_page2(self, mock_get):
         """At page 1, there a next_page, get it and return the Relationship"""
+        mock_json = make_mock_response_json({
+            "data": {
+                "paging": {
+                    "items_per_page": 8,
+                    "current_page": 2,
+                    "number_of_pages": 2,
+                    "next_page_url": None,
+                    "prev_page_url": None,
+                    "total_items": 10,
+                    "sort_order": "custom"
+                },
+                "items": [{}, {}]
+            }
+        })
+        mock_get.return_value = mock_json
+
         data = {
             "paging": {
                 "items_per_page": 8,
                 "current_page": 1,
                 "number_of_pages": 2,
                 "next_page_url": "https://api.crunchbase.com/v/2/"
-                "organization/example/past_team?user_key=123&page=2",
+                "organization/example/past_team?page=2",
                 "prev_page_url": None,
                 "total_items": 10,
                 "sort_order": "custom"
@@ -360,24 +350,13 @@ class LoadMoreTestCase(TestCase):
         }
         rs = Relationship('past_team', data)
 
-        httpretty.register_uri(
-            httpretty.GET,
-            'https://api.crunchbase.com/v/2/organization/example/past_team'
-            '?user_key=123&page=2',
-            body=json.dumps({
-                "data": {
-                    "paging": {
-                        "items_per_page": 8,
-                        "current_page": 2,
-                        "number_of_pages": 2,
-                        "next_page_url": None,
-                        "prev_page_url": None,
-                        "total_items": 10,
-                        "sort_order": "custom"
-                    },
-                    "items": [{}, {}]
-                }}))
-
         more_rs = self.cb.more(rs)
 
         self.assertEqual(2, len(more_rs))
+        mock_get.assert_called_with(
+            'https://api.crunchbase.com/v/2/organization/example/past_team'
+            '?page=2&user_key=123')
+
+
+def make_mock_response_json(dict_to_return):
+    return Mock(json=Mock(return_value=dict_to_return))
