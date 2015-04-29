2fe09cb1df270352553f687e0b20dbaf27d9e1c7
Ng Zhi An <ngzhian@gmail.com>
2015-04-29 03:25:34 +0000

    Update tests for organizations

--- tests/test_organization.py
+++ tests/test_organization.py
@@ -7,43 +7,75 @@ TEST_DATA = {
     "uuid": "df6628127f970b439d3e12f64f504fbb",
     "type": "Organization",
     "properties": {
-        "description": "Description",
-        "short_description": "short description",
-        "founded_on_day": 2,
-        "founded_on_month": 1,
-        "founded_on_year": 2012,
-        "founded_on": "2012-01-02",
         "permalink": "testpermalink",
-        "homepage_url": "http://www.example.com",
+        "api_path": "organizations/testpermalink",
+        "web_path": "organization/testpermalink",
         "name": "Organization",
-        "closed_on_day": None,
-        "closed_on_month": None,
-        "closed_on_year": None,
+        "short_description": "short description",
+        "description": "Description",
+        "primary_role": "company",
+        "role_company": True,
+        "role_investor": False,
+        "role_group": False,
+        "role_school": False,
+        "founded_on": "2012-01-02",
+        "founded_on_trust_code": "7",
+        "is_closed": False,
         "closed_on": None,
+        "closed_on_trust_code": 0,
+        "num_employees_min": 1234,
+        "num_employees_max": 2345,
         "total_funding_usd": 1234567,
         "number_of_investments": 1,
-        "stock_symbol": "COY",
-        "number_of_employees": 1234
+        "homepage_url": "http://www.example.com",
+        "created_at": 1180153335,
+        "updated_at": 1430157435,
+        "stock_symbol": "COY"
     },
     "relationships": {
-     "past_team": {
+     "current_team": {
+      "cardinality": "OneToMany",
       "paging": {
        "total_items": 3,
        "first_page_url": "https://api.crunchbase.com/v/2/\
-                          organization/example/past_team",
+                          organization/example/current_team",
        "sort_order": "created_at DESC"
       },
       "items": [
-       {
-        "first_name": "First",
-        "last_name": "Last",
-        "title": "Director of Title",
-        "started_on": None,
-        "ended_on": None,
-        "path": "person/first-last",
-        "created_at": 1234567890,
-        "updated_at": 1234567890
-       }
+          {
+              "type": "Job",
+              "uuid": "uuid123",
+              "properties": {
+                  "title": "Director of Title",
+                  "started_on": "2010-01-02",
+                  "started_on_trust_code": 0,
+                  "ended_on": None,
+                  "ended_on_trust_code": 0,
+                  "created_at": 1234567890,
+                  "updated_at": 1234567890
+              },
+              "relationships": {
+                  "person": {
+                      "type": "Person",
+                      "uuid": "uuidperson",
+                      "properties": {
+                          "permalink": "person1",
+                          "api_path": "people/person1",
+                          "web_path": "person/person1",
+                          "first_name": "Person",
+                          "last_name": "1",
+                          "role_investor": True,
+                          "born_on": "1911-01-02",
+                          "born_on_trust_code": 7,
+                          "is_deceased": False,
+                          "died_on": None,
+                          "died_on_trust_code": 0,
+                          "created_at": 1180155106,
+                          "updated_at": 1430155880
+                      }
+                  }
+              }
+          }
       ]
      }
     }
@@ -74,8 +106,7 @@ class OrganizationTestCase(TestCase):
 
     def test_organization_relationships_built(self):
         org = Organization(TEST_DATA)
-        self.assertIsNotNone(org.past_team)
-        self.assertEqual('First', org.past_team.get(0).first_name)
-        self.assertFalse(org.news)
-        self.assertFalse(org.news.get(0))
-        self.assertIsNone(org.news.get(0).first_name)
+        self.assertIsNotNone(org.current_team)
+        self.assertEqual('Director of Title', org.current_team.get(0).title)
+        # todo, relationship of a resource as a PageItem doesnt not work
+        # self.assertEqual('Person', org.current_team.get(0).person.first_name)
